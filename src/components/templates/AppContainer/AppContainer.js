90d9b797d188e1e1a8cc99ac0d354559519e99f6
Pat <plopes20587@gmail.com>
2017-12-26 18:21:05 -0500

    created table component for appContainer

--- src/components/templates/AppContainer/AppContainer.js
+++ src/components/templates/AppContainer/AppContainer.js
@@ -1,6 +1,7 @@
 // Modules
 import React from 'react';
 import Radium from 'radium';
+import Table from './Table';
 
 // Component styles
 import styles from './styles';
@@ -9,27 +10,7 @@ const AppContainer = () => (
   <div style={styles.container}>
     <div style={styles.content}>
     <h1>Rentals</h1>
-    <div id="totalDrivers"></div>
-    <table>
-      <tbody>
-        <tr>
-          <th>Status</th>
-          <th>Vehicle</th>
-          <th>Driver</th>
-          <th>Email</th>
-          <th>Start</th>
-          <th>Return</th>
-          <th>Rate</th>
-        </tr>
-        <tr>
-          <td id="">The XMLator</td>
-          <td>Standards compliance</td>
-          <td>Sloppy Code Boy</td>
-          <td>Standards compliance</td>
-          <td>Sloppy Code Boy</td>
-        </tr>
-      </tbody>
-    </table>
+    <Table />
     </div>
   </div>
 );
